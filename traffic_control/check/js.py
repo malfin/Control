c40bc2c45a2d2000cf519574c8224be4835d953c
Алексей <aiex.0115970@gmail.com>
2021-10-23 00:29:17 +0500

    update

--- traffic_control/check/js.py
+++ traffic_control/check/js.py
@@ -1,1 +1,9 @@
+import base64
+import json
 
+with open('urs.json') as jsonfile:
+    data = json.load(jsonfile)
+    datastr = json.dumps(data)
+    print(datastr)
+    encoded = base64.b64encode(datastr.encode('utf-8'))  # 1 way
+    print(encoded)
