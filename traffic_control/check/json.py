5f29b17b08ae4cbbb05f3a41a2bc883357115ac4
Алексей <aiex.0115970@gmail.com>
2021-10-22 23:20:32 +0500

    Json

---
+++ traffic_control/check/json.py
@@ -0,0 +1,40 @@
+import json
+
+data = {
+    "time": 1634747720,
+    "id": 1,
+    "rc_created_at": "2021-10-20T12:22:58.35137Z",
+    "rc_updated_at": "2021-10-20T16:35:19.545886Z",
+    "num_tl": 3228,
+    "name": "Танковый пр-д Краснокурсантский 1 пр-д ",
+    "is_enabled": True,
+    "is_alarmed": True,
+    "type": "SYNTEZ",
+    "protocol": "SCTIP",
+    "conn_status": "OK",
+    "mode": "CC_CALENDAR",
+    "program_id": 7,
+    "keep_phase_id": 0,
+    "changed_time_at": "2021-10-20T14:00:01.27971Z",
+    "real_mode": "LOCAL",
+    "real_program_id": 7,
+    "real_keep_phase_id": 0,
+    "program_created_at": "2021-10-20T12:22:58.349945Z",
+    "program_updated_at": "2021-10-20T12:22:58.349945Z",
+    "t_cycle": 90,
+    "program_type": "LOCAL",
+    "phases": [
+        {
+            "id": 1,
+            "t_osn": 32,
+            "t_prom": 6,
+            "t_min": 4,
+            "is_hidden": False,
+            "directions": [
+                1,
+                2
+            ]
+        }
+    ],
+    "start_phase_id": 1
+}
--- traffic_control/mainapp/views.py
+++ traffic_control/mainapp/views.py
@@ -2,11 +2,12 @@ import requests
 from django.shortcuts import render
 
 
+
 # https://api.via-dolorosa.ru/rc/1/full_info
 def index(request):
     try:
         response = requests.get('https://api.via-dolorosa.ru/rc/1/full_info')
-    except Exception as err:
+    except Exception:
         print('NO')
     context = {
         'title': 'главная',
