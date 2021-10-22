9782b8b7a0836fb732804c839d323ab85029abb8
Алексей <aiex.0115970@gmail.com>
2021-10-22 23:04:33 +0500

    -

--- traffic_control/mainapp/views.py
+++ traffic_control/mainapp/views.py
@@ -1,8 +1,15 @@
+import requests
 from django.shortcuts import render
 
 
+# https://api.via-dolorosa.ru/rc/1/full_info
 def index(request):
+    try:
+        response = requests.get('https://api.via-dolorosa.ru/rc/1/full_info')
+    except Exception as err:
+        print('NO')
     context = {
         'title': 'главная',
+        'response': response,
     }
     return render(request, 'mainapp/index.html', context)
