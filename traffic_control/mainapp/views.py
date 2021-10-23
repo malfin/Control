2747d03eeb885a25f969d269850801255f695f9a
pestamigo <PlagueEvgeny@yandex.ru>
2021-10-23 13:51:16 +0500

    S1

--- traffic_control/mainapp/views.py
+++ traffic_control/mainapp/views.py
@@ -14,7 +14,45 @@ def index(request):
         jsons = response.json()
         phase_id = jsons['current_phase_id']
         print(phase_id)
+        s1 = []
         # if phase_id == 1:
+        if phase_id == 1:
+            s1 = [{
+                "id": 1,
+                "t_osn": 32,
+                "t_prom": 6,
+                "t_min": 4,
+                "is_hidden": False,
+                "directions": [
+                    1,
+                    2
+                ]}]
+        elif phase_id == 2:
+            s1 = [{
+                "id": 2,
+                "t_osn": 25,
+                "t_prom": 6,
+                "t_min": 4,
+                "is_hidden": False,
+                "directions": [
+                    3
+                ]
+            }]
+        elif phase_id == 3:
+            s1 = [{
+                "id": 3,
+                "t_osn": 15,
+                "t_prom": 6,
+                "t_min": 15,
+                "is_hidden": False,
+                "directions": [
+                    4,
+                    5,
+                    6
+                ]
+            }]
+        else:
+            print('Error')
 
     except Exception:
         print('NO')
@@ -23,6 +61,7 @@ def index(request):
         'title': 'главная',
         'response': response,
         'json': jsons,
+        's1': s1,
 
     }
     return render(request, 'mainapp/index.html', context)
