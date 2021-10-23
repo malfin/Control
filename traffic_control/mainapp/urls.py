163b51dd17a8d26efc77fca55585055c206802f1
Алексей <aiex.0115970@gmail.com>
2021-10-23 14:56:31 +0500

    report errors

--- traffic_control/mainapp/urls.py
+++ traffic_control/mainapp/urls.py
@@ -4,8 +4,8 @@ import mainapp.views as mainapp
 
 app_name = 'mainapp'
 
-urlpatterns = {
+urlpatterns = [
     path('', mainapp.index, name='index'),
-    path('req1', mainapp.req1),
-    path('req2', mainapp.req2),
-}
+    path('req1', mainapp.req1, name='req1'),
+    path('req2', mainapp.req2, name='req2'),
+]
