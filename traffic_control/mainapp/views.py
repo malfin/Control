71a1f42d9bf1e4f324fddb85dd94da7e812d68c4
Алексей <aiex.0115970@gmail.com>
2021-10-23 11:19:12 +0500

    Create new track, this name - xakaton + edit html

--- traffic_control/mainapp/views.py
+++ traffic_control/mainapp/views.py
@@ -15,10 +15,46 @@ def index(request):
     except Exception:
         print('NO')
 
+    res = requests.post('https://api.via-dolorosa.ru/rc/90451/custom_phase_program', data=
+    {
+        "start_phase_id": 3,
+        "time_start_sync": 1632402791,
+        "t_cycle": 96,
+        "phases": [
+            {
+                "id": 1,
+                "t_osn": 31,
+                "t_prom": 9,
+                "t_min": 4,
+                "is_hidden": 'false',
+                "directions": 'null'
+            },
+            {
+                "id": 2,
+                "t_osn": 13,
+                "t_prom": 9,
+                "t_min": 4,
+                "is_hidden": 'false',
+                "directions": 'null'
+            },
+            {
+                "id": 3,
+                "t_osn": 25,
+                "t_prom": 9,
+                "t_min": 15,
+                "is_hidden": 'false',
+                "directions": 'null'
+            }
+        ]
+    }
+                        )
+    res.raise_for_status()
+    json_post = response.json()
     context = {
         'title': 'главная',
         'response': response,
         'json': json,
+        'json_post': json_post,
 
     }
     return render(request, 'mainapp/index.html', context)
